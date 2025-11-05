3bceb91bb5b19ba6c3a5e27c0ca94112a4b02101
Jose Luis Acevedo <you@example.com>
2025-11-04 22:54:34 -0500

    Resolve Lab1

---
+++ main.py
@@ -0,0 +1,14 @@
+# Jose Acevedo
+numero = 1
+
+while numero in range(1, 1001):
+    if (numero % 3 == 0) and (numero % 5 == 0):
+        print("Fizzbuzz")
+    elif numero % 3 == 0:
+        print("Fizz")
+    elif numero % 5 == 0:
+        print("Buzz")
+    else:
+        print(numero)
+
+    numero += 1   
